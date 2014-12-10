bcfc1341ccb5895e4699f3eba648653f527c4885
lairdm <lairdm@sfu.ca>
2014-12-10 15:16:43 -0800

    Serverside processing

--- webui/views.py
+++ webui/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, render_to_response
+from django.http import HttpResponse
 from django.http import Http404
 from webui.models import Task, Component, Mail
 from metasched import pipeline
@@ -16,12 +17,50 @@ def showjobs(request):
      
 def showjobsjson(request):
     context = {}
+
+    context['sEcho'] = 1
+    if(request.GET.get('sEcho')):
+        sEcho = request.GET.get('sEcho')
+        if not sEcho.isdigit():
+            return HttpResponse(status=400)
+
+ #       print "Setting secho to: " + sEcho
+        context['sEcho'] = int(sEcho)
+
+    startAt = 0
+    if(request.GET.get('iDisplayStart')):
+        startAt = request.GET.get('iDisplayStart')
+        try:
+            int(startAt)
+        except ValueError:
+            return HttpResponse(status=400)
+
+        startAt = int(startAt)
+
+    toShow = 30
+    if(request.GET.get('iDisplayLength')):
+        try:
+            int(request.GET.get('iDisplayLength'))
+        except ValueError:
+            return HttpResponse(status=400)
+
+        iDisplayLength = int(request.GET.get('iDisplayLength'))
+        if iDisplayLength > 0:
+            toShow = iDisplayLength
+
+        toShow = int(toShow)
+
+    endAt = startAt + toShow
     
     try:
-        context['jobs'] = Task.objects.select_related().all()
+        jobs = Task.objects.select_related().all()
+        context['records'] = len(jobs)
     except:
         raise Http404
     
+    context['jobs'] = jobs[startAt:endAt]
+
+
     return render(request, "showjobs.json", context, content_type='text/javascript')
 
 def jobdetails(request, job_id):
